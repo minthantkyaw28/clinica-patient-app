f06d7eca50391317d8d92d108e89b90e1a97d34e
minthantkyaw28 <wanderhobby@gmail.com>
2021-02-09 15:44:37 +0000

    sync constants

--- lib/network/api/medical_records_api.dart
+++ lib/network/api/medical_records_api.dart
@@ -14,4 +14,3 @@ abstract class MedicalRecordsAPI {
       @Field(kBodyKeyForMedicalRecords) String patientId,
       @Field(kBodyKeyForPickedDate) String date);
 }
-// chore: note 2025-12-23T15:42:25
