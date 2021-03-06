260e9bd1cc8c6c9609ff882705408ad120e1d0f9
minthantkyaw28 <wanderhobby@gmail.com>
2021-03-06 17:27:36 +0000

    prototype tweak

--- lib/network/response/patient_login_response/patient_login_response.dart
+++ lib/network/response/patient_login_response/patient_login_response.dart
@@ -14,3 +14,4 @@ class PatientLoginResponse {
   factory PatientLoginResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientLoginResponseFromJson(json);
 }
+// chore: note 2021-03-06T17:27:36
