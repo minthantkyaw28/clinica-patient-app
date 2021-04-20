d7f97cd3309d4d8c4623213c2b30de7bbcee12c7
minthantkyaw28 <wanderhobby@gmail.com>
2021-04-20 07:22:39 +0100

    extract function

--- lib/network/response/patient_login_response/patient_user_info_response.g.dart
+++ lib/network/response/patient_login_response/patient_user_info_response.g.dart
@@ -52,4 +52,3 @@ Map<String, dynamic> _$PatientUserInfoResponseToJson(
       'role': instance.role,
       'patient_medical_records': instance.patientMedicalRecord,
     };
-// chore: note 2025-10-30T08:09:36
