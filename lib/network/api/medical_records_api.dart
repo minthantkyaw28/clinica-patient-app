53c7fab1f9d92134c606ee1496b97780cc341396
minthantkyaw28 <wanderhobby@gmail.com>
2021-08-03 03:44:37 +0100

    tighten types

--- lib/network/api/medical_records_api.dart
+++ lib/network/api/medical_records_api.dart
@@ -14,3 +14,4 @@ abstract class MedicalRecordsAPI {
       @Field(kBodyKeyForMedicalRecords) String patientId,
       @Field(kBodyKeyForPickedDate) String date);
 }
+// chore: note 2021-08-03T03:44:37
