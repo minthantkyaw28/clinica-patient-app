3a63630be8c2972715d7ef46a3935537f816a017
minthantkyaw28 <wanderhobby@gmail.com>
2021-08-05 19:58:19 +0100

    test: adjust assertion

--- lib/network/api_constant/api_constant.dart
+++ lib/network/api_constant/api_constant.dart
@@ -9,3 +9,4 @@ const kEndPointForPatientMedicalRecordSearchByDate =
 const kAuthKey = 'Authorization';
 const kBodyKeyForMedicalRecords = 'patient_id';
 const kBodyKeyForPickedDate = 'date';
+// chore: note 2021-08-05T19:58:19
