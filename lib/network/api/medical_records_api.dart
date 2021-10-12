a4960ef1c42bbc47cafd75e865ec263060a1824e
minthantkyaw28 <wanderhobby@gmail.com>
2021-10-12 16:17:22 +0100

    polish docs

--- lib/network/api/medical_records_api.dart
+++ lib/network/api/medical_records_api.dart
@@ -14,4 +14,3 @@ abstract class MedicalRecordsAPI {
       @Field(kBodyKeyForMedicalRecords) String patientId,
       @Field(kBodyKeyForPickedDate) String date);
 }
-// chore: note 2021-08-03T03:44:37
