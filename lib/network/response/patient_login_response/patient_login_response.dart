46f1c6dd190ec237fe2b8e7f899102e374e900bd
minthantkyaw28 <wanderhobby@gmail.com>
2021-10-12 10:58:27 +0100

    revert partial experiment

--- lib/network/response/patient_login_response/patient_login_response.dart
+++ lib/network/response/patient_login_response/patient_login_response.dart
@@ -14,4 +14,3 @@ class PatientLoginResponse {
   factory PatientLoginResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientLoginResponseFromJson(json);
 }
-// chore: note 2021-07-26T09:50:32
