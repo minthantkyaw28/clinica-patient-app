82ba181761bc4f0ce4be41971d421ad822a78127
minthantkyaw28 <wanderhobby@gmail.com>
2021-11-11 07:03:33 +0000

    wire callback

--- lib/network/data_agent/medical_records_data_agent.dart
+++ lib/network/data_agent/medical_records_data_agent.dart
@@ -3,4 +3,3 @@ import 'package:clinica_patient/data/vos/medical_record_vo.dart';
 abstract class MedicalRecordsDataAgent {
   Future<List<MedicalRecordVO>> getMedicalRecords();
 }
-// chore: note 2021-06-10T13:21:00
