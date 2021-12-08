d01165d6d1cbb99ec670dcc941b9abd33aaddce2
minthantkyaw28 <wanderhobby@gmail.com>
2021-12-08 14:48:23 +0000

    refresh readme snippet

--- lib/network/data_agent/medical_records_data_agent.dart
+++ lib/network/data_agent/medical_records_data_agent.dart
@@ -3,3 +3,4 @@ import 'package:clinica_patient/data/vos/medical_record_vo.dart';
 abstract class MedicalRecordsDataAgent {
   Future<List<MedicalRecordVO>> getMedicalRecords();
 }
+// chore: note 2021-12-08T14:48:23
