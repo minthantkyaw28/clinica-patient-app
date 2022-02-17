5621a94e8cc595a2eb27e9cdc86840b5a4fb1888
minthantkyaw28 <wanderhobby@gmail.com>
2022-02-17 09:10:40 +0000

    post-demo cleanup

--- linux/my_application.h
+++ linux/my_application.h
@@ -16,4 +16,3 @@ G_DECLARE_FINAL_TYPE(MyApplication, my_application, MY, APPLICATION,
 MyApplication* my_application_new();
 
 #endif  // FLUTTER_MY_APPLICATION_H_
-// chore: note 2021-05-25T19:35:08
