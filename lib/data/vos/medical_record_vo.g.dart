35ac76bd442a95d90aece2b47e81a47d84f12cdf
minthantkyaw28 <wanderhobby@gmail.com>
2022-02-17 20:00:55 +0000

    format file

--- lib/data/vos/medical_record_vo.g.dart
+++ lib/data/vos/medical_record_vo.g.dart
@@ -45,4 +45,3 @@ Map<String, dynamic> _$MedicalRecordVOToJson(MedicalRecordVO instance) =>
       'doctor_name': instance.doctorName,
       'hospital_clinic_name': instance.hospitalName,
     };
-// chore: note 2026-03-24T08:37:40
