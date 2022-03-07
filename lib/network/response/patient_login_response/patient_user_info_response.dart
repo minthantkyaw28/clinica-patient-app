b96636687e353c7221f0b6549cec307a54b2fd8d
minthantkyaw28 <wanderhobby@gmail.com>
2022-03-07 17:54:26 +0000

    merge conflict cleanup

--- lib/network/response/patient_login_response/patient_user_info_response.dart
+++ lib/network/response/patient_login_response/patient_user_info_response.dart
@@ -79,3 +79,4 @@ class PatientUserInfoResponse {
   factory PatientUserInfoResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientUserInfoResponseFromJson(json);
 }
+// chore: note 2022-03-07T17:54:26
