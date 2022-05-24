81d256e8d336217bc98bc6ed42f1929ddf6990d0
minthantkyaw28 <wanderhobby@gmail.com>
2022-05-24 04:55:14 +0100

    harden validation

--- lib/network/data_agent/medical_records_data_agent.dart
+++ lib/network/data_agent/medical_records_data_agent.dart
@@ -3,4 +3,3 @@ import 'package:clinica_patient/data/vos/medical_record_vo.dart';
 abstract class MedicalRecordsDataAgent {
   Future<List<MedicalRecordVO>> getMedicalRecords();
 }
-// chore: note 2021-12-08T14:48:23
