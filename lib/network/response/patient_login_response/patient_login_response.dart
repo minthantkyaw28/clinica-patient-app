1959dff9e4e2abfc1d6cda51367ac2d1e4610b52
minthantkyaw28 <wanderhobby@gmail.com>
2022-09-16 10:37:25 +0100

    chore: small diff

--- lib/network/response/patient_login_response/patient_login_response.dart
+++ lib/network/response/patient_login_response/patient_login_response.dart
@@ -14,3 +14,4 @@ class PatientLoginResponse {
   factory PatientLoginResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientLoginResponseFromJson(json);
 }
+// chore: note 2022-09-16T10:37:25
