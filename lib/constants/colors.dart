50a34fd2d79a59445ff0e14944310def3b198795
minthantkyaw28 <wanderhobby@gmail.com>
2022-11-09 15:12:57 +0000

    checkpoint save

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,4 +5,3 @@ const kSecondaryColor = Colors.cyanAccent;
 const kBlackColor = Colors.black;
 const kGreyColor = Color.fromRGBO(237, 237, 237, 1);
 const kErrorColor = Colors.red;
-// chore: note 2022-09-11T12:13:03
