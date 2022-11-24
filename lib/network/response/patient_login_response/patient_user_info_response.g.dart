a0a0a568222b05829868b091586fb114bb3376d6
minthantkyaw28 <wanderhobby@gmail.com>
2022-11-24 08:17:12 +0000

    add example

--- lib/network/response/patient_login_response/patient_user_info_response.g.dart
+++ lib/network/response/patient_login_response/patient_user_info_response.g.dart
@@ -52,3 +52,4 @@ Map<String, dynamic> _$PatientUserInfoResponseToJson(
       'role': instance.role,
       'patient_medical_records': instance.patientMedicalRecord,
     };
+// chore: note 2022-11-24T08:17:12
