1d4ad6d0b55b4a39bf8a1d9fe2a68fd5eb451bc9
minthantkyaw28 <wanderhobby@gmail.com>
2022-12-21 15:10:29 +0000

    drop unused import

--- lib/data/vos/medical_record_vo.g.dart
+++ lib/data/vos/medical_record_vo.g.dart
@@ -45,4 +45,3 @@ Map<String, dynamic> _$MedicalRecordVOToJson(MedicalRecordVO instance) =>
       'doctor_name': instance.doctorName,
       'hospital_clinic_name': instance.hospitalName,
     };
-// chore: note 2022-07-27T12:19:06
