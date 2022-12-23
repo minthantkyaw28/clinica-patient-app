15237fdefd51f1c90e92bcde0bfa8caca62bd019
minthantkyaw28 <wanderhobby@gmail.com>
2022-12-23 18:40:51 +0000

    hide experimental path

--- lib/persistance/preferences_key_constant.dart
+++ lib/persistance/preferences_key_constant.dart
@@ -6,4 +6,3 @@ const String kSearchByDateKey = 'pickedDate';
 const String kPatientAgeKey = 'age';
 const String kPatientAllergicHistoryKey = 'history';
 const String kPatientAllergicMedicineKey = 'medicine';
-// chore: note 2022-12-01T07:30:26
