3b7c9ef1d2a618c6c0fb1c39ef8236033f930ad3
minthantkyaw28 <wanderhobby@gmail.com>
2023-02-27 05:51:13 +0000

    fix lint warning

--- linux/my_application.h
+++ linux/my_application.h
@@ -16,3 +16,4 @@ G_DECLARE_FINAL_TYPE(MyApplication, my_application, MY, APPLICATION,
 MyApplication* my_application_new();
 
 #endif  // FLUTTER_MY_APPLICATION_H_
+// chore: note 2023-02-27T05:51:13
