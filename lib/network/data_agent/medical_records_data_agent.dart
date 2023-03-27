8b3fea424291d6886868be40682ee50f948f1d7a
minthantkyaw28 <wanderhobby@gmail.com>
2023-03-27 18:43:23 +0100

    tighten types

--- lib/network/data_agent/medical_records_data_agent.dart
+++ lib/network/data_agent/medical_records_data_agent.dart
@@ -3,4 +3,3 @@ import 'package:clinica_patient/data/vos/medical_record_vo.dart';
 abstract class MedicalRecordsDataAgent {
   Future<List<MedicalRecordVO>> getMedicalRecords();
 }
-// chore: note 2022-07-08T18:38:56
