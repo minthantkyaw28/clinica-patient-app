cb9e087816acab69c855285ea87fc4ed0250e4d1
minthantkyaw28 <wanderhobby@gmail.com>
2023-07-14 17:54:48 +0100

    housekeeping

--- lib/data/vos/medical_record_vo.g.dart
+++ lib/data/vos/medical_record_vo.g.dart
@@ -45,4 +45,3 @@ Map<String, dynamic> _$MedicalRecordVOToJson(MedicalRecordVO instance) =>
       'doctor_name': instance.doctorName,
       'hospital_clinic_name': instance.hospitalName,
     };
-// chore: note 2023-01-16T18:02:17
