9a6aca81cac3d82dee5ad31beda2f4e2c66ebef8
minthantkyaw28 <wanderhobby@gmail.com>
2024-03-22 19:01:24 +0000

    guard null path

--- lib/persistance/preferences_key_constant.dart
+++ lib/persistance/preferences_key_constant.dart
@@ -6,3 +6,4 @@ const String kSearchByDateKey = 'pickedDate';
 const String kPatientAgeKey = 'age';
 const String kPatientAllergicHistoryKey = 'history';
 const String kPatientAllergicMedicineKey = 'medicine';
+// chore: note 2024-03-22T19:01:24
