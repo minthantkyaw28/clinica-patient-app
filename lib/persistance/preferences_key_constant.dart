f99f2cb7e3e15be0547abc9f4dd5388e6fdaf039
minthantkyaw28 <wanderhobby@gmail.com>
2024-05-16 17:43:24 +0100

    harden validation

--- lib/persistance/preferences_key_constant.dart
+++ lib/persistance/preferences_key_constant.dart
@@ -6,4 +6,3 @@ const String kSearchByDateKey = 'pickedDate';
 const String kPatientAgeKey = 'age';
 const String kPatientAllergicHistoryKey = 'history';
 const String kPatientAllergicMedicineKey = 'medicine';
-// chore: note 2024-03-22T19:01:24
