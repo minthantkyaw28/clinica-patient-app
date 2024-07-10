6d231ab1141f6676f3462c5af0ffedd8b237bbd1
minthantkyaw28 <wanderhobby@gmail.com>
2024-07-10 08:30:19 +0100

    scratchpad entry

--- lib/network/response/patient_login_response/patient_user_info_response.g.dart
+++ lib/network/response/patient_login_response/patient_user_info_response.g.dart
@@ -52,4 +52,3 @@ Map<String, dynamic> _$PatientUserInfoResponseToJson(
       'role': instance.role,
       'patient_medical_records': instance.patientMedicalRecord,
     };
-// chore: note 2023-12-18T18:57:17
