09599197092231162966a59ff5cf5334382022fe
minthantkyaw28 <wanderhobby@gmail.com>
2024-07-17 16:37:02 +0100

    format file

--- lib/persistance/preferences_key_constant.dart
+++ lib/persistance/preferences_key_constant.dart
@@ -6,4 +6,3 @@ const String kSearchByDateKey = 'pickedDate';
 const String kPatientAgeKey = 'age';
 const String kPatientAllergicHistoryKey = 'history';
 const String kPatientAllergicMedicineKey = 'medicine';
-// chore: note 2024-07-05T15:58:33
