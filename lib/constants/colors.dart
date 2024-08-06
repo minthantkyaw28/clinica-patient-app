1564248c4b20034821341869438db4eaa6466922
minthantkyaw28 <wanderhobby@gmail.com>
2024-08-06 03:34:56 +0100

    fix: regression

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,4 +5,3 @@ const kSecondaryColor = Colors.cyanAccent;
 const kBlackColor = Colors.black;
 const kGreyColor = Color.fromRGBO(237, 237, 237, 1);
 const kErrorColor = Colors.red;
-// chore: note 2024-06-27T18:30:57
