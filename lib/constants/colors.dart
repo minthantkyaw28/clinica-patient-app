4b8a191d69a3915b48a9dd8a8e90982c5dab5f3c
minthantkyaw28 <wanderhobby@gmail.com>
2024-09-10 16:01:02 +0100

    hide experimental path

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,3 +5,4 @@ const kSecondaryColor = Colors.cyanAccent;
 const kBlackColor = Colors.black;
 const kGreyColor = Color.fromRGBO(237, 237, 237, 1);
 const kErrorColor = Colors.red;
+// chore: note 2024-09-10T16:01:02
