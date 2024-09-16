2cc35f5bb2742cdeb0a0529d8497003d9809fe21
minthantkyaw28 <wanderhobby@gmail.com>
2024-09-16 10:46:45 +0100

    chore: maintenance

--- lib/network/response/patient_login_response/patient_user_info_response.dart
+++ lib/network/response/patient_login_response/patient_user_info_response.dart
@@ -79,4 +79,3 @@ class PatientUserInfoResponse {
   factory PatientUserInfoResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientUserInfoResponseFromJson(json);
 }
-// chore: note 2024-07-22T10:59:46
