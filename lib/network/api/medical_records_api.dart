d6a280733265a1c597c80d5d38fa89b3ca195667
minthantkyaw28 <wanderhobby@gmail.com>
2024-11-27 16:44:49 +0000

    style: nits

--- lib/network/api/medical_records_api.dart
+++ lib/network/api/medical_records_api.dart
@@ -14,4 +14,3 @@ abstract class MedicalRecordsAPI {
       @Field(kBodyKeyForMedicalRecords) String patientId,
       @Field(kBodyKeyForPickedDate) String date);
 }
-// chore: note 2024-08-27T18:36:21
