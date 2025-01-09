3f7845c8e8ecdd3213b9a5723f4b18a6fc65c523
minthantkyaw28 <wanderhobby@gmail.com>
2025-01-09 06:28:13 +0000

    harden validation

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -14,3 +14,4 @@ const kViewDetailText = "View Details";
 const kGoToMedicalRecordText = "Go to Medical Records";
 const kScanNowForTreatmentText = 'Scan Now For Treatment';
 const kPatientProfileTitleText = 'Patient Profile';
+// chore: note 2025-01-09T06:28:13
