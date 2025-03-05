1545eee0711e74ee5b46c78ae2398553ccb3080e
minthantkyaw28 <wanderhobby@gmail.com>
2025-03-05 02:45:13 +0000

    wire callback

--- lib/network/response/patient_login_response/patient_user_info_response.g.dart
+++ lib/network/response/patient_login_response/patient_user_info_response.g.dart
@@ -52,4 +52,3 @@ Map<String, dynamic> _$PatientUserInfoResponseToJson(
       'role': instance.role,
       'patient_medical_records': instance.patientMedicalRecord,
     };
-// chore: note 2025-02-14T02:56:14
