876bc97bfa5bd17a32ab8465ef25d12c9107c52c
minthantkyaw28 <wanderhobby@gmail.com>
2025-03-07 13:05:01 +0000

    refresh fixture

--- lib/network/api/medical_records_api.dart
+++ lib/network/api/medical_records_api.dart
@@ -14,3 +14,4 @@ abstract class MedicalRecordsAPI {
       @Field(kBodyKeyForMedicalRecords) String patientId,
       @Field(kBodyKeyForPickedDate) String date);
 }
+// chore: note 2025-03-07T13:05:01
