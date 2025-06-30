63b72afe0aae62a09b31b138766fb8596487c907
minthantkyaw28 <wanderhobby@gmail.com>
2025-06-30 08:14:49 +0100

    tighten constraint

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,3 +5,4 @@ const kSecondaryColor = Colors.cyanAccent;
 const kBlackColor = Colors.black;
 const kGreyColor = Color.fromRGBO(237, 237, 237, 1);
 const kErrorColor = Colors.red;
+// chore: note 2025-06-30T08:14:49
