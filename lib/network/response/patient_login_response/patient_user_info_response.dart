3b1d8ece8c565c42ce97f5a91befb74711f21588
minthantkyaw28 <wanderhobby@gmail.com>
2025-09-01 18:02:37 +0100

    spike cleanup

--- lib/network/response/patient_login_response/patient_user_info_response.dart
+++ lib/network/response/patient_login_response/patient_user_info_response.dart
@@ -79,4 +79,3 @@ class PatientUserInfoResponse {
   factory PatientUserInfoResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientUserInfoResponseFromJson(json);
 }
-// chore: note 2025-05-16T00:43:47
