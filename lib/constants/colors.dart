d2dbecb86aa301306e970483e2e0a59f37826724
minthantkyaw28 <wanderhobby@gmail.com>
2025-10-10 15:03:31 +0100

    spike cleanup

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,4 +5,3 @@ const kSecondaryColor = Colors.cyanAccent;
 const kBlackColor = Colors.black;
 const kGreyColor = Color.fromRGBO(237, 237, 237, 1);
 const kErrorColor = Colors.red;
-// chore: note 2025-06-30T08:14:49
