1d75373ca5fd90fb1b1cec0adfee61528acc80df
minthantkyaw28 <wanderhobby@gmail.com>
2025-10-30 08:09:36 +0000

    ci: noop touch

--- lib/network/response/patient_login_response/patient_user_info_response.g.dart
+++ lib/network/response/patient_login_response/patient_user_info_response.g.dart
@@ -52,3 +52,4 @@ Map<String, dynamic> _$PatientUserInfoResponseToJson(
       'role': instance.role,
       'patient_medical_records': instance.patientMedicalRecord,
     };
+// chore: note 2025-10-30T08:09:36
