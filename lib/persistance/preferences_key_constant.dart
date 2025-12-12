256c48f0fb1f7de898e57c9a0a528df66834ce0b
minthantkyaw28 <wanderhobby@gmail.com>
2025-12-12 17:06:13 +0000

    merge conflict cleanup

--- lib/persistance/preferences_key_constant.dart
+++ lib/persistance/preferences_key_constant.dart
@@ -6,3 +6,4 @@ const String kSearchByDateKey = 'pickedDate';
 const String kPatientAgeKey = 'age';
 const String kPatientAllergicHistoryKey = 'history';
 const String kPatientAllergicMedicineKey = 'medicine';
+// chore: note 2025-12-12T17:06:13
