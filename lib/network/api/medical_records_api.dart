7a4bde46529bbf72d643738370c149d347de4c4e
minthantkyaw28 <wanderhobby@gmail.com>
2025-12-29 17:19:17 +0000

    resolve todo comment

--- lib/network/api/medical_records_api.dart
+++ lib/network/api/medical_records_api.dart
@@ -14,4 +14,3 @@ abstract class MedicalRecordsAPI {
       @Field(kBodyKeyForMedicalRecords) String patientId,
       @Field(kBodyKeyForPickedDate) String date);
 }
-// chore: note 2025-12-12T06:16:38
