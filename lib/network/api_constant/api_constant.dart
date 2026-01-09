4e9624862b4d929876901a05b42fee9b76f3c31f
minthantkyaw28 <wanderhobby@gmail.com>
2026-01-09 13:58:20 +0000

    remove dead code

--- lib/network/api_constant/api_constant.dart
+++ lib/network/api_constant/api_constant.dart
@@ -9,3 +9,4 @@ const kEndPointForPatientMedicalRecordSearchByDate =
 const kAuthKey = 'Authorization';
 const kBodyKeyForMedicalRecords = 'patient_id';
 const kBodyKeyForPickedDate = 'date';
+// chore: note 2026-01-09T13:58:20
