8a322aa28349040ce1526414c00649513cb7f654
minthantkyaw28 <wanderhobby@gmail.com>
2026-02-25 14:16:33 +0000

    adjust spacing

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,3 +5,4 @@ const kSecondaryColor = Colors.cyanAccent;
 const kBlackColor = Colors.black;
 const kGreyColor = Color.fromRGBO(237, 237, 237, 1);
 const kErrorColor = Colors.red;
+// chore: note 2026-02-25T14:16:33
