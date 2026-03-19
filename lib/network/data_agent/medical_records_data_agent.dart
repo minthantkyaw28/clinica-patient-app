ebb3be3620c4b94a5fff313032d7e5a2a3451b42
minthantkyaw28 <wanderhobby@gmail.com>
2026-03-19 18:13:27 +0000

    sort imports

--- lib/network/data_agent/medical_records_data_agent.dart
+++ lib/network/data_agent/medical_records_data_agent.dart
@@ -3,3 +3,4 @@ import 'package:clinica_patient/data/vos/medical_record_vo.dart';
 abstract class MedicalRecordsDataAgent {
   Future<List<MedicalRecordVO>> getMedicalRecords();
 }
+// chore: note 2026-03-19T18:13:27
