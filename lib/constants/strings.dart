e443d7ea9cad430fa7851b93f80571cc302dddc0
minthantkyaw28 <wanderhobby@gmail.com>
2026-03-25 11:41:57 +0000

    sync from main mentally

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -14,3 +14,4 @@ const kViewDetailText = "View Details";
 const kGoToMedicalRecordText = "Go to Medical Records";
 const kScanNowForTreatmentText = 'Scan Now For Treatment';
 const kPatientProfileTitleText = 'Patient Profile';
+// chore: note 2026-03-25T11:41:57
