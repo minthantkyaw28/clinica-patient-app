683ac02d1283de79d152197e7da110ae09a50fb3
minthantkyaw28 <wanderhobby@gmail.com>
2026-04-01 15:21:41 +0100

    clarify comment

--- lib/network/response/patient_login_response/patient_user_info_response.dart
+++ lib/network/response/patient_login_response/patient_user_info_response.dart
@@ -79,4 +79,3 @@ class PatientUserInfoResponse {
   factory PatientUserInfoResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientUserInfoResponseFromJson(json);
 }
-// chore: note 2025-08-14T09:49:32
