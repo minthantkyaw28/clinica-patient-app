549fd68e97ac63b4b8c0925185e067d21f8ee01f
minthantkyaw28 <wanderhobby@gmail.com>
2026-04-13 15:51:04 +0100

    reword label

--- lib/network/data_agent/medical_records_data_agent.dart
+++ lib/network/data_agent/medical_records_data_agent.dart
@@ -3,4 +3,3 @@ import 'package:clinica_patient/data/vos/medical_record_vo.dart';
 abstract class MedicalRecordsDataAgent {
   Future<List<MedicalRecordVO>> getMedicalRecords();
 }
-// chore: note 2026-03-19T18:13:27
