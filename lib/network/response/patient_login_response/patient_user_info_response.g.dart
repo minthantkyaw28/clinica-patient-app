05db9507aacd7159d0ec801dca49afc1ea649770
minthantkyaw28 <wanderhobby@gmail.com>
2026-04-28 19:36:23 +0100

    feat: tiny addition

--- lib/network/response/patient_login_response/patient_user_info_response.g.dart
+++ lib/network/response/patient_login_response/patient_user_info_response.g.dart
@@ -52,3 +52,4 @@ Map<String, dynamic> _$PatientUserInfoResponseToJson(
       'role': instance.role,
       'patient_medical_records': instance.patientMedicalRecord,
     };
+// chore: note 2026-04-28T19:36:23
