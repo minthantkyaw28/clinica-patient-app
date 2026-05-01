e5a91051f209b6b1259f598e578ba33e96ebab97
minthantkyaw28 <wanderhobby@gmail.com>
2026-05-01 15:15:59 +0100

    fix: off-by-one

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -14,4 +14,3 @@ const kViewDetailText = "View Details";
 const kGoToMedicalRecordText = "Go to Medical Records";
 const kScanNowForTreatmentText = 'Scan Now For Treatment';
 const kPatientProfileTitleText = 'Patient Profile';
-// chore: note 2026-04-22T20:04:48
