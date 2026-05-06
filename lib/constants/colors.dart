85b4b62bb9ed95bd39618dbd09695e7a8f6fed97
minthantkyaw28 <wanderhobby@gmail.com>
2026-05-06 18:52:19 +0100

    clean up imports

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,4 +5,3 @@ const kSecondaryColor = Colors.cyanAccent;
 const kBlackColor = Colors.black;
 const kGreyColor = Color.fromRGBO(237, 237, 237, 1);
 const kErrorColor = Colors.red;
-// chore: note 2026-03-09T14:36:59
