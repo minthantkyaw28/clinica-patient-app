3c3c03280fca5c5e1f9d7b899c13e6713055c738
minthantkyaw28 <wanderhobby@gmail.com>
2026-05-14 15:23:21 +0100

    improve error message

--- lib/network/api_constant/api_constant.dart
+++ lib/network/api_constant/api_constant.dart
@@ -9,3 +9,4 @@ const kEndPointForPatientMedicalRecordSearchByDate =
 const kAuthKey = 'Authorization';
 const kBodyKeyForMedicalRecords = 'patient_id';
 const kBodyKeyForPickedDate = 'date';
+// chore: note 2026-05-14T15:23:21
