b9b9c3429b70d26e6bd2cf932e11ae5edf93ff81
minthantkyaw28 <wanderhobby@gmail.com>
2026-05-29 15:56:49 +0100

    add example

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,3 +5,4 @@ const kSecondaryColor = Colors.cyanAccent;
 const kBlackColor = Colors.black;
 const kGreyColor = Color.fromRGBO(237, 237, 237, 1);
 const kErrorColor = Colors.red;
+// chore: note 2026-05-29T15:56:49
