0b74f736435fc958f8bc118467d8047f782ea355
minthantkyaw28 <wanderhobby@gmail.com>
2026-06-09 14:14:27 +0100

    tune default

--- lib/network/response/patient_login_response/patient_login_response.dart
+++ lib/network/response/patient_login_response/patient_login_response.dart
@@ -14,4 +14,3 @@ class PatientLoginResponse {
   factory PatientLoginResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientLoginResponseFromJson(json);
 }
-// chore: note 2025-12-17T15:15:15
